a6e62e5692aabc668ed5166c02b58c1afbbc7a4d
15872120491 <2292441626@qq.com>
2025-02-26 22:18:56 +0800

    初始化项目

---
+++ src/main/java/com/fz/fzpicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.fz.fzpicturebackend.mapper;
+
+import com.fz.fzpicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author fang
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-02-26 19:35:05
+* @Entity com.fz.fzpicturebackend.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
